133b124e4fb35ee0f4bbc93ee3986fa001a4bd04
zzp <2664871975@qq.com>
2024-04-11 13:41:48 +0800

    zzp init

--- zzp-generator-240411/src/main/java/com/zzp/Main.java
+++ zzp-generator-240411/src/main/java/com/zzp/Main.java
@@ -3,5 +3,11 @@ package com.zzp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("zzp946");
+        }
+
+
     }
 }
