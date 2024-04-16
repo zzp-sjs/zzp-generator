7faedc89aa1998324d7c5af3818e1899f9e2145a
zzp <2664871975@qq.com>
2024-04-16 14:52:05 +0800

    zzp 240416-在FreeMarkerTest类中的test方法中设置数字格式

--- zzp-generator-240411/src/test/java/FreeMarkerTest.java
+++ zzp-generator-240411/src/test/java/FreeMarkerTest.java
@@ -1,9 +1,12 @@
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,7 +19,7 @@ import java.util.Map;
 
 public class FreeMarkerTest {
     @Test
-    public void test() throws IOException {
+    public void test() throws IOException, TemplateException {
         // new 出 Configuration 对象，参数为 FreeMarker 版本号
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_32);
 
@@ -27,12 +30,30 @@ public class FreeMarkerTest {
         configuration.setDefaultEncoding("utf-8");
 
         // 创建模板对象，加载指定模板
-        Template template = configuration.getTemplate("myweb.html.ftl");
+        Template template = configuration.getTemplate("zzp.html.ftl");
+
+        //设置数字格式
+        configuration.setNumberFormat("0.######");
 
         //创建数据类型
         Map<String, Object> dataModel = new HashMap<>();
         dataModel.put("currentYear",2023);
         List<Map<String, Object>> menuItems = new ArrayList<>();
+        Map<String, Object> menuItem1 = new HashMap<>();
+        menuItem1.put("url","https://www.baidu.com/");
+        menuItem1.put("lable","百度");
+        Map<String, Object> menuItem2 = new HashMap<>();
+        menuItem2.put("url","https://www.qq.com");
+        menuItem2.put("lable","QQ");
+        menuItems.add(menuItem1);
+        menuItems.add(menuItem2);
+        dataModel.put("menuItems", menuItems);
+
+        //生成
+        Writer out = new FileWriter("zzp.ftl");
+        template.process(dataModel,out);
+
+        out.close();
     }
 
 
---
+++ zzp-generator-240411/zzp.ftl
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>zzp946</title>
+</head>
+<body>
+<h1>欢迎来到ZZP乐园</h1>
+<ul>
+            <li><a href="https://www.baidu.com/">百度</a></li>
+        <li><a href="https://www.qq.com">QQ</a></li>
+</ul>
+<footer>
+    2023 unbelievablezZZP All Right Reserved.
+</footer>
+</body>
+</html>
