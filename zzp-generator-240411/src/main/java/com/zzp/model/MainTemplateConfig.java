8429d2e89744b7742e4d0567282b566a545fb2d9
zzp <2664871975@qq.com>
2024-04-16 15:09:00 +0800

    zzp 240416-在model中新建动态模板配置类MainTemplateConfig.java

---
+++ zzp-generator-240411/src/main/java/com/zzp/model/MainTemplateConfig.java
@@ -0,0 +1,32 @@
+package com.zzp.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: zzp
+ * @Date: 2024/04/16/下午3:02
+ * @Description:
+ */
+
+/**
+ * 动态模板配置
+ */
+public class MainTemplateConfig {
+
+    /**
+     *是否生成循环
+     */
+    private boolean loop;
+
+    /**
+     * 作者注释
+     */
+    private String author = "zzp";
+
+    /**
+     * 输出信息
+     */
+    private String outputText = "sum = ";
+
+
+}
